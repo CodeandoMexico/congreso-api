766d753e33a09d9ccfface99b5ae0fdeca13b709
Adrian Rangel <adrian.rangel@gmail.com>
2014-05-29 12:05:49 -0500

    index lists initiatives controller

--- app/controllers/v1/votes_controller.rb
+++ app/controllers/v1/votes_controller.rb
@@ -1,7 +1,8 @@
 module V1
   class VotesController < ApplicationController
     def index
-
+      @votes = Vote.where("initiative_id = ?", params[:initiative_id])
+      render json: @votes
     end
   end
 end
