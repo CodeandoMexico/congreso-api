20af575f4b1a551880055760d4fe958909a7b9cd
Adrian Rangel <adrian.rangel@gmail.com>
2014-06-08 16:48:45 -0500

    deputy model relations

--- app/models/deputy.rb
+++ app/models/deputy.rb
@@ -1,2 +1,4 @@
 class Deputy < ActiveRecord::Base
+  has_many :votes
+  has_many :initiatives, through: :votes
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,4 @@
 class Vote < ActiveRecord::Base
   belongs_to :initiative
+  belongs_to :deputy
 end
