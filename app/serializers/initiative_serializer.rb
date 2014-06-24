c68c4cfd000c6503e6d9eda593db6e50ca14d281
Adrian Rangel <adrian.rangel@gmail.com>
2014-06-24 18:43:27 -0500

    quitar cantidad_votos

--- app/serializers/initiative_serializer.rb
+++ app/serializers/initiative_serializer.rb
@@ -1,7 +1,3 @@
 class InitiativeSerializer < ActiveModel::Serializer
-  attributes :id, :decree, :date, :period, :year, :legislature, :cantidad_votos
-
-  def cantidad_votos
-    object.votes.count
-  end
+  attributes :id, :decree, :date, :period, :year, :legislature
 end
