22c0f4fb031819812aa3f6dfffe051bab3499be5
Adrian Rangel <adrian.rangel@gmail.com>
2014-07-03 03:29:07 -0500

    allow acces control headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::API
   def default_serializer_options
     { root: false }
   end
+
+  private
+
+  # This is used to allow the cross origin requests
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Request-Method'] = %w(GET POST OPTIONS).join(',')
+  end
 end
