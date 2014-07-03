0e23b384a24f46e1fe3fab45529f135b7d8b2d03
Adrian Rangel <adrian.rangel@gmail.com>
2014-07-03 03:31:07 -0500

    deputies allow acces control headers

--- app/controllers/v1/deputies_controller.rb
+++ app/controllers/v1/deputies_controller.rb
@@ -11,5 +11,13 @@ module V1
       render json: @top, each_serializer: DeputyTopSerializer#, tipo_voto: params[:tipo_voto]
       # render json: @top
     end
+
+    private
+
+    # This is used to allow the cross origin requests
+    def set_access_control_headers
+      headers['Access-Control-Allow-Origin'] = "*"
+      headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
+    end
   end
 end
